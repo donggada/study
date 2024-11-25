c6461c33000f4fa0f9c6f09ab004dd830dca973d
donggada <koem0425@naver.com>
2024-11-25 22:45:23 +0900

    Update CouponIssueService.java

--- mvcTraffic/src/main/java/com/side/mvcTraffic/domain/coupon/service/CouponIssueService.java
+++ mvcTraffic/src/main/java/com/side/mvcTraffic/domain/coupon/service/CouponIssueService.java
@@ -4,4 +4,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CouponIssueService {
+
+    public void issue(long couponId, long userId) {
+
+    }
+
+    private void issueRequest(long couponId, long userId) {
+    }
 }
