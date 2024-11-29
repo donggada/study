cc3db941e65bbce37b4bdac82979bbe75d8bff0f
donggada <koem0425@naver.com>
2024-11-29 19:37:33 +0900

    쿠폰 발급 레거시 코드 작성

--- mvcTraffic/src/main/java/com/side/mvcTraffic/domain/coupon/service/CouponIssueService.java
+++ mvcTraffic/src/main/java/com/side/mvcTraffic/domain/coupon/service/CouponIssueService.java
@@ -1,14 +1,2 @@
-package com.side.mvcTraffic.domain.coupon.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CouponIssueService {
-
-    public void issue(long couponId, long userId) {
-
-    }
-
-    private void issueRequest(long couponId, long userId) {
-    }
+package com.side.mvcTraffic.domain.coupon.service;public interface CouponIssueService {
 }
---
+++ mvcTraffic/src/main/java/com/side/mvcTraffic/domain/coupon/service/CouponIssueServiceImp.java
@@ -0,0 +1,2 @@
+package com.side.mvcTraffic.domain.coupon.service;public class CouponIssueServiceImp {
+}
